2e957ba6f55d623b53f395910cddfe999c352778
MoYuanJun <moyuanjun925@gmail.coml>
2023-11-08 20:21:39 +0800

    /api/cache-control

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const Koa = require('koa');
+const moment = require('moment');
+const cors = require('@koa/cors')
+const Router = require('@koa/router');
+
+const app = new Koa();
+const router = new Router();
+
+// 跨域设置
+app.use(cors({
+  maxAge: 5,
+  origin: "*",
+  credentials: true,
+  allowMethods: ['GET', 'POST'],
+  allowHeaders: ['Content-Type'],
+  exposeHeaders: ['Content-Type'],
+}));
+
+// 1. expires
+router.get('/api/expires', async (ctx) => {
+  ctx.status = 200;
+  ctx.body = 111111111;
+  ctx.set('expires', `${moment().utc().add(2, 'm').format('ddd, DD MMM YYYY HH:mm:ss')} GMT`);
+});
+
+// 2. Cache-Control
+router.get('/api/cache-control', async (ctx) => {
+  ctx.status = 200;
+  ctx.body = 111111111;
+  ctx.set('Cache-Control', `max-age=120`);
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000);
